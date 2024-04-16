54b8aa8fd03505d061ea777fe9d823424d8bf72b
IrshadHussain4866 <166916871+IrshadHussain4866@users.noreply.github.com>
2024-04-16 03:02:01 -0700

    this is first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+list1=[1,2,3,4,5,7,9,4,2,0]
+print(list1)
+
+print(list1)
+
+
+
---
+++ new.py
@@ -0,0 +1,11 @@
+class new:
+    name="irshad"
+    def __init__(self,name):
+     self.name=name
+    
+        
+s1=new("sabir")
+print(s1.name)
+print(new.name)
+del s1.name
+print(s1.name)
